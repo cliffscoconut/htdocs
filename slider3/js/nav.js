cba5021fc5aebbe29c4f3d1fb68b8b68dd323430
cliffscoconut <cliffordhuang18@gmail.com>
2015-06-22 19:02:24 -0700

    6-22-15
    homework

--- slider3/js/nav.js
+++ slider3/js/nav.js
@@ -3,24 +3,54 @@ var navElement = document.getElementById('nav');
 var containerElement = document.getElementById('container');
 
 // containerElement.style.marginLeft = "0 px";
+     containerElement.style.marginLeft = "0px";
 
 hamburgerElement.addEventListener("click", function() {
-  console.log(containerElement);
+  console.log(containerElement.style.marginLeft);
+
+
   // console.log(containerElement.style.display);
-
-  if(navElement.style.display == "")
-    {
-      navElement.style.display = "block";
-      console.log(containerElement.style.marginLeft);
-      containerElement.style.marginLeft = containerElement.style.marginLeft + navElement.width + "px";
-      console.log(containerElement.style.marginLeft);
-    }
-  else if(navElement.style.display == "block")
-    {
-      navElement.style.display = "";
-      containerElement.style.marginLeft = containerElement.style.marginLeft - navElement.width + "px";
-    }
+  // if(navElement.style.display == "")
+  //   {
+      // navElement.style.display = "block";
+      // console.log(containerElement.style.marginLeft);
 
 
+      if(containerElement.style.marginLeft == "0px")
+        {
+        containerElement.style.marginLeft = navElement.clientWidth + "px";
+        } else if(containerElement.style.marginLeft = navElement.clientWidth)
+        {
+        containerElement.style.marginLeft = "0px";
+        }
 
-})
+
+  //     // console.log(containerElement.style.marginLeft);
+  //   }
+  // else if(navElement.style.display == "block")
+  //   {
+  //     navElement.style.display = "";
+  //     containerElement.style.marginLeft = containerElement.style.marginLeft - navElement.clientWidth + "px";
+  //   }
+  //
+
+
+});
+
+// navState = false;
+//
+// var navInit = function(){
+//
+//   hamburger.addEventListner('click',function(){
+//     if(navState===true){
+//       navElement.style.marginLeft = 0 + '%';
+//       navState = false;
+//     } else if(navState===false){
+//       navElement.style.marginLeft = 15 + '%';
+//       navState = true;
+//     }
+//
+//   });
+// };
+//
+// navInit();
